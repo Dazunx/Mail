2411ca6382c46b2dd34f6d480e44ff20962e8e22
Dazunx <22100493.handong.ac.kr>
2022-07-14 15:45:46 +0900

    Create MailBox

---
+++ lib/home.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import 'mail/mail_list/mail_screen.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( // 위젯?
+      backgroundColor: Color(0xFFF2F2F6),
+      body: SafeArea( // 안전하게 화면 안에 다 들어가게 설정
+        child: ListView ( //스크롤 가능
+          children: [ //children은 여러 위젯 => 대괄호로 감싸야함.
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          CupertinoIcons.square_arrow_left,
+                          size: 20,
+                          color: Colors.blueAccent,
+                        ),),
+                      Text(
+                        "편집",
+                        style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontSize: 15,
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "메일상자",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          _mainButton(CupertinoIcons.tray,"받은 편지함"),
+                          Divider(indent: 50, color: Colors.grey),
+                          _mainButton(CupertinoIcons.star,"VIP"),
+                          Divider(indent: 50, color: Colors.grey),
+                          _mainButton(CupertinoIcons.flag,"깃발 표시됨"),
+                        ]
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Gmail",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ]
+                  ),
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 8),
+                          color: Colors.white,
+                          child: Column(
+                            children: [
+                              _mainButton(CupertinoIcons.doc,"임시 저장"),
+                              Divider(indent: 50, color: Colors.grey),
+                              _mainButton(CupertinoIcons.paperplane, "보낸 편지함"),
+                              Divider(indent: 50, color: Colors.grey),
+                              _mainButton(CupertinoIcons.bin_xmark, "정크"),
+                              Divider(indent: 50, color: Colors.grey),
+                              _mainButton(CupertinoIcons.delete, "휴지통"),
+                              Divider(indent: 50, color: Colors.grey),
+                              _mainButton(CupertinoIcons.archivebox, "모든 메일"),
+                              Divider(indent: 50, color: Colors.grey),
+                              _mainButton(CupertinoIcons.folder, "별표 편지함"),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget _mainButton(IconData icon, String title) {
+  return GestureDetector(
+    child: Padding(
+      padding: const EdgeInsets.fromLTRB(15.0,2.0,15.0,0),
+      child:  Stack( alignment: AlignmentDirectional.centerStart,
+        children: [
+          Row(
+            children: [
+              Icon(
+                icon,
+                size: 20,
+                  color: Colors.blueAccent,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Text(
+                title,
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(
+                CupertinoIcons.chevron_compact_right,
+                size: 15,
+                color: Colors.grey,
+              )
+            ],
+          ),
+        ],
+      ),
+    ),
+
+    onTap: (){
+      Get.to(MailScreen(title));
+    },
+  );
+}
